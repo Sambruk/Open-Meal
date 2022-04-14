82b9383863ccc5983e6404380e2085dacbfbba5c
Rickard Svensson <rickard.svensson@mashie.se>
2022-04-14 10:25:26 +0200

    Fix broken links after main was made default

--- js/wizard.js
+++ js/wizard.js
@@ -1,70 +1,94 @@
-$(function() {	
-	var openMealDataProviderURL = 'https://raw.githubusercontent.com/Sambruk/Open-Meal/master/dataproviders.json';
+$(function () {
+  var openMealDataProviderURL =
+    "https://raw.githubusercontent.com/Sambruk/Open-Meal/main/dataproviders.json";
 
-	// Populate initial data provider drop down
-  	var dataProiderXHR = $.getJSON(openMealDataProviderURL, function(response){  		
-  		$.each(response.data, function(i, provider){
-  			$('#openMealWizard #dataProviders').append(
-    			$('<option />').text(provider.name)
-    						   .val(provider.name)
-    						   .attr('data-language', provider.lang)
-    						   .attr('data-baseurl', provider.baseUrl)
-    		);
-  		})
-  		$('#openMealWizard #dataProvidersContainer .loadingImage').hide();
-  		$('#openMealWizard #dataProviders').show();  		
-  	})
-  	.fail(function() {
-	    $('#openMealWizard #distributorsContainer .loadingImage').hide();
-	    $('#openMealWizard #openMealWizard .alert').html('<strong>Error:</strong> Could not get data providers').show();
-	});
+  // Populate initial data provider drop down
+  var dataProiderXHR = $.getJSON(openMealDataProviderURL, function (response) {
+    $.each(response.data, function (i, provider) {
+      $("#openMealWizard #dataProviders").append(
+        $("<option />")
+          .text(provider.name)
+          .val(provider.name)
+          .attr("data-language", provider.lang)
+          .attr("data-baseurl", provider.baseUrl)
+      );
+    });
+    $("#openMealWizard #dataProvidersContainer .loadingImage").hide();
+    $("#openMealWizard #dataProviders").show();
+  }).fail(function () {
+    $("#openMealWizard #distributorsContainer .loadingImage").hide();
+    $("#openMealWizard #openMealWizard .alert")
+      .html("<strong>Error:</strong> Could not get data providers")
+      .show();
+  });
 
-  	// -- Event Listeners --
+  // -- Event Listeners --
 
-  	$('#openMealWizard #dataProviders').change(function(){
-  		$('#openMealWizard #widgetCodeContainer').hide(); 			   				
-		$('#openMealWizard #distributors').find("option:gt(0)").remove();
-		$('#openMealWizard #distributors').hide();
-		$('#openMealWizard .alert').hide();
+  $("#openMealWizard #dataProviders").change(function () {
+    $("#openMealWizard #widgetCodeContainer").hide();
+    $("#openMealWizard #distributors").find("option:gt(0)").remove();
+    $("#openMealWizard #distributors").hide();
+    $("#openMealWizard .alert").hide();
 
-  		var currentOption = $("#openMealWizard #dataProviders option:selected");
-  		if (currentOption.val() == '') return;  	
+    var currentOption = $("#openMealWizard #dataProviders option:selected");
+    if (currentOption.val() == "") return;
 
-  		$('#openMealWizard #distributorsContainer').show();	
-  		$('#openMealWizard #distributorsContainer .loadingImage').show();
-  		
-  		// Populate distributor drop down
-  		var distributorXHR = $.getJSON(currentOption.data('baseurl') + '/openmeal/v2/distributors.json', function(response){  		
-  			$.each(response.data, function(i, distributor){
-  				$('#openMealWizard #distributors').append(
-    				$('<option />').text(distributor.name + ' (' + distributor.address.addressLocality + ')')
-    						   	   .val(distributor.distributorID)
-    						   	   .attr('data-language', distributor.lang)    						   	
-    			);
-  			})
+    $("#openMealWizard #distributorsContainer").show();
+    $("#openMealWizard #distributorsContainer .loadingImage").show();
 
-  			$('#openMealWizard #distributorsContainer .loadingImage').hide();
-  			$('#openMealWizard #distributors').show();    					
-  		})
-  		.fail(function() {
-	    	$('#openMealWizard #distributorsContainer .loadingImage').hide();
-	    	$('#openMealWizard .alert').html('<strong>Error:</strong> Could not get distributors from ' + currentOption.val()).show();
-	  	});
-  	});
+    // Populate distributor drop down
+    var distributorXHR = $.getJSON(
+      currentOption.data("baseurl") + "/openmeal/v2/distributors.json",
+      function (response) {
+        $.each(response.data, function (i, distributor) {
+          $("#openMealWizard #distributors").append(
+            $("<option />")
+              .text(
+                distributor.name +
+                  " (" +
+                  distributor.address.addressLocality +
+                  ")"
+              )
+              .val(distributor.distributorID)
+              .attr("data-language", distributor.lang)
+          );
+        });
 
-  	$('#openMealWizard #distributors').change(function(){  		
-  		$('#openMealWizard .alert').hide();
+        $("#openMealWizard #distributorsContainer .loadingImage").hide();
+        $("#openMealWizard #distributors").show();
+      }
+    ).fail(function () {
+      $("#openMealWizard #distributorsContainer .loadingImage").hide();
+      $("#openMealWizard .alert")
+        .html(
+          "<strong>Error:</strong> Could not get distributors from " +
+            currentOption.val()
+        )
+        .show();
+    });
+  });
 
-  		var currentOption = $("#openMealWizard #distributors option:selected");
-  		if (currentOption.val() == '') return;
+  $("#openMealWizard #distributors").change(function () {
+    $("#openMealWizard .alert").hide();
 
-  		var currentProvider = $("#openMealWizard #dataProviders option:selected");  		
-  		var openMealURL = currentProvider.data('baseurl') + '/openmeal/v2/meals.json?distributorID=' + currentOption.val();
-  		var code = '<div id="openMealWidget" data-language="' + currentOption.data('language') + '" data-openmealurl="' + openMealURL + '"></div>\n' +  		
-    			   '<script src="https://sambruk.github.io/Open-Meal/js/widget.js" id="openMealWidgetScript" data-widgets="openMealWidget"></script>';
+    var currentOption = $("#openMealWizard #distributors option:selected");
+    if (currentOption.val() == "") return;
 
-		$('#openMealWizard #openMealWidgetCode code').text(code);   		
+    var currentProvider = $("#openMealWizard #dataProviders option:selected");
+    var openMealURL =
+      currentProvider.data("baseurl") +
+      "/openmeal/v2/meals.json?distributorID=" +
+      currentOption.val();
+    var code =
+      '<div id="openMealWidget" data-language="' +
+      currentOption.data("language") +
+      '" data-openmealurl="' +
+      openMealURL +
+      '"></div>\n' +
+      '<script src="https://sambruk.github.io/Open-Meal/js/widget.js" id="openMealWidgetScript" data-widgets="openMealWidget"></script>';
 
-		$('#openMealWizard #widgetCodeContainer').show(); 			   		
-  	});
+    $("#openMealWizard #openMealWidgetCode code").text(code);
+
+    $("#openMealWizard #widgetCodeContainer").show();
+  });
 });
